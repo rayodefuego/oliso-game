2b0791b75b86513c8effacc3d973b5e2f035c7b0
rayodefuego <fabianestebandelatorre@gmail.com>
2024-05-12 15:51:48 -0500

    se creo la base para la función askForMove

--- src/oliso/ConsoleUI.java
+++ src/oliso/ConsoleUI.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class ConsoleUI {
 
-  private Scanner scanner;
 
   public ConsoleUI() {
-    scanner = new Scanner(System.in);
 
   }
 
   public static void main(String[] args) {
     Game game = new Game(4);
+    Scanner scanner = new Scanner(System.in);
     printBoard(game.getBoard());
+    askForMove(scanner);
   }
 
   private static void printBoard(int[][] board) {
@@ -33,6 +33,66 @@ public class ConsoleUI {
       if (i != board.length - 1) {
         System.out.print("-----\n");
       }
+    }
+  }
+
+  private static void askForMove(Scanner scanner){
+    System.out.println("player turn");
+    int xPos = askForXPosition(scanner);
+    int yPos = askForYPosition(scanner);
+    int size = askForPieceSize(scanner);
+    System.out.println("xPos: " + xPos + ", yPos: " + yPos + ", size: " + size);
+  }
+  private static int askForXPosition(Scanner scanner){
+    int xPos;
+    System.out.println("Please enter the x position of the move:");
+    try{
+      xPos = Integer.parseInt(scanner.nextLine());
+    }catch (NumberFormatException ex){
+      System.out.println("This is not a valid number, please try again");
+      return askForXPosition(scanner);
+    }
+
+    if(xPos < 0 || xPos > 2){
+      System.out.println("This is not a valid number, please try again");
+      return askForXPosition(scanner);
+    }
+
+    return xPos;
+  }
+  private static int askForYPosition(Scanner scanner){
+    int yPos;
+    System.out.println("Please enter the y position of the move:");
+    try{
+      yPos = Integer.parseInt(scanner.nextLine());
+    }catch (NumberFormatException ex){
+      System.out.println("This is not a valid number, please try again");
+      return askForYPosition(scanner);
+    }
+
+    if(yPos < 0 || yPos > 2){
+      System.out.println("This is not a valid number, please try again");
+      return askForYPosition(scanner);
+    }
+
+    return yPos;
+  }
+  private  static int askForPieceSize(Scanner scanner){
+    System.out.println("s{Small}, m{Medium}, b{Big}");
+    System.out.println("Please enter the size of the piece");
+
+    String size = scanner.nextLine();
+
+    switch (size){
+      case "s", "S", "small", "Small":
+        return 0;
+      case "m", "M", "medium", "Medium":
+        return 1;
+      case "b", "B", "big", "Big":
+        return 2;
+      default:
+        System.out.println(size + "is not a valid input, please try again");
+        return askForPieceSize(scanner);
     }
   }
 }
