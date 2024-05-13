f1e354eb846ff6a404ef25b8ce037f2d73051eac
alejito1 <159492969+alejito1@users.noreply.github.com>
2024-05-12 19:01:41 -0500

    Documentation and boardchecker update
    Updated documentation adn boardchecker

--- src/oliso/BoardChecker.java
+++ src/oliso/BoardChecker.java
@@ -1,8 +1,14 @@
 package oliso;
 
 public class BoardChecker {
-  private boolean checkForSpace1() {
-    return true;
+  private boolean checkForWin(int[][] board) {
+    if(checkForSmall(board)) return true;
+    if(checkForMedium(board)) return true;
+    if(checkForBig(board)) return true;
+    if(checkForBigToSmall(board)) return true;
+    if(checkForSmallToBig(board)) return true;
+
+    return false;
 
   }
 
@@ -21,5 +27,102 @@ public class BoardChecker {
       }
     }
     return false;
+  }
+
+  private static int getSmall(int num3) {
+    return num3%10;
+  }
+  private static int getMedium(int num3) {
+    return ((num3-(num3%10))%100)/10;
+  }
+  private static int getBig(int num3) {
+    return num3/100;
+  }
+  private static boolean checkForSmall (int [][] board) {
+    for (int i = 0; i < board.length; i++) {
+      if ((getSmall(board[i][0]) == getSmall(board[i][1])) && (getSmall(board[i][0]) == getSmall(board[i][2]))){
+        return true;
       }
+      if ((getSmall(board[0][i]) == getSmall(board[1][i])) && (getSmall(board[0][i]) == getSmall(board[2][i]))){
+        return true;
+      }
+    }
+    if ((getSmall(board[0][0]) == getSmall(board[1][1])) && (getSmall(board[0][0]) == getSmall(board[2][2]))){
+      return true;
+    }
+    if ((getSmall(board[0][2]) == getSmall(board[1][1])) && (getSmall(board[0][2]) == getSmall(board[2][0]))){
+      return true;
+    }
+    return false;
+  }
+  private static boolean checkForMedium (int [][] board) {
+    for (int i = 0; i < board.length; i++) {
+      if ((getMedium(board[i][0]) == getMedium(board[i][1])) && (getMedium(board[i][0]) == getMedium(board[i][2]))){
+        return true;
+      }
+      if ((getMedium(board[0][i]) == getMedium(board[1][i])) && (getMedium(board[0][i]) == getMedium(board[2][i]))){
+        return true;
+      }
+    }
+    if ((getMedium(board[0][0]) == getMedium(board[1][1])) && (getMedium(board[0][0]) == getMedium(board[2][2]))){
+      return true;
+    }
+    if ((getMedium(board[0][2]) == getMedium(board[1][1])) && (getMedium(board[0][2]) == getMedium(board[2][0]))){
+      return true;
+    }
+    return false;
+  }
+  private static boolean checkForBig (int [][] board) {
+    for (int i = 0; i < board.length; i++) {
+      if ((getBig(board[i][0]) == getBig(board[i][1])) && (getBig(board[i][0]) == getBig(board[i][2]))){
+        return true;
+      }
+      if ((getBig(board[0][i]) == getBig(board[1][i])) && (getBig(board[0][i]) == getBig(board[2][i]))){
+        return true;
+      }
+    }
+    if ((getBig(board[0][0]) == getBig(board[1][1])) && (getBig(board[0][0]) == getBig(board[2][2]))){
+      return true;
+    }
+    if ((getBig(board[0][2]) == getBig(board[1][1])) && (getBig(board[0][2]) == getBig(board[2][0]))){
+      return true;
+    }
+    return false;
+  }
+  private static boolean checkForBigToSmall(int[][]board) {
+    for (int i = 0; i < board.length; i++) {
+      if ((getBig(board[i][0]) == getMedium(board[i][1])) && (getBig(board[i][0]) == getSmall(board[i][2]))){
+        return true;
+      }
+      if ((getBig(board[0][i]) == getMedium(board[1][i])) && (getBig(board[0][i]) == getSmall(board[2][i]))){
+        return true;
+      }
+    }
+    if ((getBig(board[0][0]) == getMedium(board[1][1])) && (getBig(board[0][0]) == getSmall(board[2][2]))){
+      return true;
+    }
+    if ((getBig(board[0][2]) == getMedium(board[1][1])) && (getBig(board[0][2]) == getSmall(board[2][0]))){
+      return true;
+    }
+    return false;
+
+  }
+  private static boolean checkForSmallToBig(int[][]board) {
+    for (int i = 0; i < board.length; i++) {
+      if ((getSmall(board[i][0]) == getMedium(board[i][1])) && (getSmall(board[i][0]) == getBig(board[i][2]))){
+        return true;
+      }
+      if ((getSmall(board[0][i]) == getMedium(board[1][i])) && (getSmall(board[0][i]) == getBig(board[2][i]))){
+        return true;
+      }
+    }
+    if ((getSmall(board[0][0]) == getMedium(board[1][1])) && (getSmall(board[0][0]) == getBig(board[2][2]))){
+      return true;
+    }
+    if ((getSmall(board[0][2]) == getMedium(board[1][1])) && (getSmall(board[0][2]) == getBig(board[2][0]))){
+      return true;
+    }
+    return false;
+
+  }
 }
